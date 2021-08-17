282682b09af09341c045a9201dd90c140702980c
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-08-18 02:57:43 +0900

    [ADD] #1 - Init Home View

---
+++ feature/home/src/main/java/com/java/home/recommand/RecommendFragment.kt
@@ -0,0 +1,19 @@
+package com.java.home.recommand
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.java.home.R
+
+class RecommendFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recommend, container, false)
+    }
+}
