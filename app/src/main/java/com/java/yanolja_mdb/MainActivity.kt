5042895055352249f25442e8bc95ffaf04495acd
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-08-18 16:21:18 +0900

    [ADD] #7 - Init MainActivity

--- app/src/main/java/com/java/yanolja_mdb/MainActivity.kt
+++ app/src/main/java/com/java/yanolja_mdb/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.java.yanolja_mdb
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.java.core.base.BindingActivity
+import com.java.core.util.replace
+import com.java.home.EmptyFragment
+import com.java.home.HomeFragment
+import com.java.yanolja_mdb.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val homeFragment: HomeFragment by lazy { HomeFragment() }
+    private val emptyFragment: EmptyFragment by lazy { EmptyFragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        replaceHomeFragment()
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    replaceHomeFragment()
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    replaceEmptyFragment()
+                    return@setOnItemSelectedListener true
+                }
+            }
+        }
+    }
+
+    private fun replaceHomeFragment() {
+        replace(R.id.fragmentContainerView, homeFragment)
+    }
+
+    private fun replaceEmptyFragment() {
+        replace(R.id.fragmentContainerView, emptyFragment)
     }
 }
